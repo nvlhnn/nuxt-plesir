5da7fc5d978ee82bd55819dac122f5279a4c6185
nvlhnn <hanannaufal17@gmail.com>
2023-09-19 13:47:32 +0700

    changes api url

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://go-plesir.herokuapp.com/api',
+    baseURL: 'https://go-plesir.fly.dev/api',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
